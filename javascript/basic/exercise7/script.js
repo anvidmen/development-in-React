2e26469d437976d7771efef848e85edfc1f6df20
Juan David Om <anvidmen@gmail.com>
2020-10-05 15:35:26 +0200

    exercise, list of objects

---
+++ javascript/basic/exercise7/script.js
@@ -0,0 +1,6 @@
+const objects = ['table', 'chair', 'computer', 'notebook']
+
+for (let i = 0; i < objects.length; i++) {
+  console.log(`The ${objects[i]} object is at position ${[i]}`)
+  // console.log('The ' + objects[i] + ' object is at position ' + [i])
+}
