a2ad31903cae62f606d7163fe95cc266f69d3308
Juan David Om <anvidmen@gmail.com>
2020-10-05 12:27:57 +0200

    exercise, add between two numbers

---
+++ javascript/basic/exercise4/script.js
@@ -0,0 +1,7 @@
+var number1 = 34
+var number2 = 56
+
+var total = number1 + number2
+
+console.log('The sum between ' + number1 + ' and ' + number2 + ' is ' + total + '.')
+// console.log(`The sum between ${number1} and ${number2} is ${total}.`)
