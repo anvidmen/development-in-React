f7e8042d483c849e1f691512d833242731a5f387
Juan David Om <anvidmen@gmail.com>
2020-12-02 06:58:47 +0100

    Added structural part, logic and styles of the app

---
+++ react/tic-toc-toe/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Game } from './components/Game'
+import Footer from './components/Footer'
+import './App.sass'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Game />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
